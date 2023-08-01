53f689cfaf20db85d740177a90077cc6ff29af74
ryans <raycosm@gmail.com>
2023-08-01 13:10:36 -0600

    Added RpgClass to Character, renamed Class to RpgClass

--- dotnet-rpg/dotnet-rpg/Models/Character.cs
+++ dotnet-rpg/dotnet-rpg/Models/Character.cs
@@ -8,6 +8,7 @@
         public int Strength { get; set; } = 10;
         public int Defense { get; set; } = 10;
         public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; }
 
     }
 }
