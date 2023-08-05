f38a801a9eb921cb5e8ed47112f8095a81aea7fd
ryans <raycosm@gmail.com>
2023-08-04 21:50:16 -0600

    Defined authentication interface

---
+++ dotnet-rpg/dotnet-rpg/Data/AuthRepository.cs
@@ -0,0 +1,29 @@
+﻿using dotnet_rpg.Models;
+using dotnet_rpg.Services;
+
+namespace dotnet_rpg.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dotnet-rpg/dotnet-rpg/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+﻿using dotnet_rpg.Models;
+using dotnet_rpg.Services;
+
+namespace dotnet_rpg.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+    }
+}
